9cf0ea5acf4998c9df59bbb15db0436ff96abe7c
Samch1k <sav20006@gmail.com>
2025-05-17 02:08:08 +0300

    Added chainlit.yaml configuration and simplified main.py

--- chainlit_app/main.py
+++ chainlit_app/main.py
@@ -1,10 +1,6 @@
 import chainlit as cl
 from llm import nl_to_sql, ask_llm
 from db import execute_sql
-import os
-
-# Set default environment variables
-os.environ.setdefault('PORT', '8080')
 
 @cl.on_message
 async def main(message: cl.Message):
